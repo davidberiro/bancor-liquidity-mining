867d337f030fd5f67e2efea0aa9648abb16b0d52
Natanael Prudhomme <nat@liquidapps.io>
2021-10-05 14:19:24 -0400

    add http to debug

--- debug/debug.js
+++ debug/debug.js
@@ -1,18 +1,34 @@
 const Web3 = require('web3');
-const web3 = new Web3(`ws://${process.env.WEB3_PROVIDER}:8545`);
+const web3A = new Web3(`http://${process.env.WEB3_PROVIDER}:8545`);
+const web3B = new Web3(`ws://${process.env.WEB3_PROVIDER}:8545`);
 
 const dappStakingPoolABI = require('../abi/DappStakingPool.json');
-const contract = new web3.eth.Contract(dappStakingPoolABI, "0x05Aa229Aec102f78CE0E852A812a388F076Aa555");
+const contractA = new web3A.eth.Contract(dappStakingPoolABI, "0x05Aa229Aec102f78CE0E852A812a388F076Aa555");
+const contractB = new web3B.eth.Contract(dappStakingPoolABI, "0x05Aa229Aec102f78CE0E852A812a388F076Aa555");
 
 (async () => {
-    await contract.methods.userPoolTotalEntries(0).call(
+    await contractA.methods.userPoolTotalEntries(0).call(
     {
         from: '0x05Aa229Aec102f78CE0E852A812a388F076Aa555'
     }, function(error, result){
         console.log(result);
         console.log(error);
     });
-    await contract.methods.poolInfo(0).call(
+    await contractA.methods.poolInfo(0).call(
+    {
+        from: '0x05Aa229Aec102f78CE0E852A812a388F076Aa555'
+    }, function(error, result){
+        console.log(result);
+        console.log(error);
+    });
+    await contractB.methods.userPoolTotalEntries(0).call(
+    {
+        from: '0x05Aa229Aec102f78CE0E852A812a388F076Aa555'
+    }, function(error, result){
+        console.log(result);
+        console.log(error);
+    });
+    await contractB.methods.poolInfo(0).call(
     {
         from: '0x05Aa229Aec102f78CE0E852A812a388F076Aa555'
     }, function(error, result){
