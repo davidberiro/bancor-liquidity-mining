c3fbe08e49b82fb937e905b423125a35caffbf88
Natanael Prudhomme <nat@liquidapps.io>
2021-10-28 13:55:43 -0400

    add deploy / transfer owner scripts

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -10,20 +10,24 @@ async function main() {
 
   // We get the contract to deploy
   const dappStakingPoolFactory = await hre.ethers.getContractFactory("DappStakingPool");
-  const dappTokenFactory = await hre.ethers.getContractFactory("DappToken");
+  const funderFactory = await hre.ethers.getContractFactory("Funder");
 
-  const dappTokenContract = await dappTokenFactory.deploy();
-  await dappTokenContract.deployed();
-  console.log("Dapp Token deployed to:", dappTokenContract.address);
+  const dappStakingPoolProxy = await upgrades.deployProxy(dappStakingPoolFactory, [
+    "0x853c2D147a1BD7edA8FE0f58fb3C5294dB07220e", // liquidity protection
+    "0xf5FAB5DBD2f3bf675dE4cB76517d4767013cfB55", // liq protection store
+    "0x33A23d447De16a8Ff802c9Fcc917465Df01A3977", // dapp bnt anchor
+    "0x939b462ee3311f8926c047d2b576c389092b1649", // dapp token
+    "0x1f573d6fb3f13d689ff844b4ce37794d79a7ff1c", // bnt token
+    await hre.ethers.provider.getBlockNumber(), // start block
+    205000 // 20.5 DAPPs per block with 4 decimal precision
+  ]);
+  console.log("dapp Staking Pool Proxy deployed to:", dappStakingPoolProxy.address);
 
-  const dappStakingPoolContract = await dappStakingPoolFactory.deploy();
-  await dappStakingPoolContract.deployed();
-  console.log("Dapp Staking Pool deployed to:", dappStakingPoolContract.address);
-
+  // start at 0% for rewards so all goes to IL
+  const funderProxy = await upgrades.deployProxy(funderFactory, [dappStakingPoolProxy.address,"0x939b462ee3311f8926c047d2b576c389092b1649",0]);
+  console.log("funder Proxy deployed to:", funderProxy.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main()
   .then(() => process.exit(0))
   .catch(error => {
