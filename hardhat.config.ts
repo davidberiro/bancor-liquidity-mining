b83102e09f745dfebf7315e3d03dced452af0730
helios138 <73124365+helios138@users.noreply.github.com>
2022-05-10 09:20:22 +0800

    upgrade solc version to 0.8.13

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -15,7 +15,7 @@ const hardhatConfig: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: "0.8.2",
+        version: "0.8.13",
         settings: {
           optimizer: {
             enabled: true,
